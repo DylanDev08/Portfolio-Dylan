a5c762528f5aa96e95d0e59796b8f0f18209d66c
DylanDev08 <dylansalcedo333@gmail.com>
2026-09-09 11:22:55 -0300

    ux: reset scroll position between routes

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,10 +1,12 @@
 import { AppRouter } from "./AppRouter";
 import { ErrorBoundary } from "../components/common/ErrorBoundary";
 import { SeoManager } from "../components/common/SeoManager";
+import { ScrollToTop } from "../components/common/ScrollToTop";
 
 export function App() {
   return (
     <ErrorBoundary>
+      <ScrollToTop />
       <SeoManager />
       <AppRouter />
     </ErrorBoundary>
